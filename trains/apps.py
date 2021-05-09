706560fa1e108b91160c46159adb4e7518aa3cee
stigsanek <stigsanek@yandex.ru>
2021-05-09 13:30:58 +0400

    Adds train model

--- trains/apps.py
+++ trains/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TrainsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trains'
+    verbose_name = 'Поезда'
--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from cities.models import City
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Поезд')
+    travel_time = models.PositiveIntegerField(verbose_name='Время в пути/ч')
+    from_city = models.ForeignKey(
+        to=City,
+        on_delete=models.CASCADE,
+        related_name='from_city_set',
+        verbose_name='Город отправления'
+    )
+    to_city = models.ForeignKey(
+        to='cities.City',
+        on_delete=models.CASCADE,
+        related_name='to_city_set',
+        verbose_name='Город прибытия'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Поезд'
+        verbose_name_plural = 'Поезда'
+        ordering = ['travel_time']
